f9b0a6fcdb01cca1c76f447399723948d97d7555
айкеновад <@234_11>
2018-06-12 09:04:29 +0600

    Add project files.

---
+++ Module3_Lesson3_/Source.cpp
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#include<math.h>
+#include<time.h>
+#include <Windows.h>
+#include <conio.h>
+
+void main()
+{
+
+	setlocale(LC_ALL, "Rus");
+
+	srand(time(NULL));
+
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	int tn = 0;
+
+start:
+
+	printf("task number:\t");
+	scanf("%d", &tn);
+
+	switch (tn)
+	{
+	case 1:
+	{
+
+	}
+	break;
+	case 2:
+	{
+
+	}
+	break;
+	case 3:
+	{
+
+	}
+	break;
+	case 4:
+	{
+
+	}
+	break;
+	case 5:
+	{
+
+	}
+	break;
+	case 6:
+	{
+
+	}
+	break;
+	case 7:
+	{
+
+	}
+	break;
+	default:
+		printf("\nerror: no tasks\n\n");
+	}
+	goto start;
+}
